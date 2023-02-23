7c4bc9a635e1f57fac272da7adc8e6c423de1bf4
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-02-23 12:04:09 +0000

    Add props

--- reactivationator/src/App.js
+++ reactivationator/src/App.js
@@ -25,14 +25,14 @@
 
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps />
+      {/* <FunctionalGreeting /> */}
+      <FunctionalGreetingWithProps greeting = "how's it going?" age = "40" address = "Saggart" dog = "Benji" wife = "Aline"/>
     </div>
   );
 }
--- reactivationator/src/components/FunctionalGreetingWithProps.js
+++ reactivationator/src/components/FunctionalGreetingWithProps.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const FunctionalGreetingWithProps = () => <h1>Hello, Sam U Well!</h1>;
+// const FunctionalGreetingWithProps() = (props) => <h1>Hello, Sam U Well!</h1>;
+
+const FunctionalGreetingWithProps = (props) => {
+    return (<h1>Hello, Sam U Well! {props.greeting} you crazy {props.age} year old eejit living in {props.address} with your dog {props.dog} and wife {props.wife}</h1>);
+}
 
 export default FunctionalGreetingWithProps;
